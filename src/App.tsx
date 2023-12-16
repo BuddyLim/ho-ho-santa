aa4066a0d37d25607d7f5cf075dc7e3a95af2ea8
Buddy <buddy.tlimk@gmail.com>
2023-12-16 10:39:26 +0800

    [Add] Bootstrapping

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  useEffect(() =>{
+    fetch(import.meta.env.VITE_S3_URL)
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+  }, [])
 
   return (
     <>
